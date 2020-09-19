84171fdf552c69f4d887a510cf38eeaf24658cbe
MirusD <damir.salihov@inbox.ru>
2020-09-19 17:15:02 +0500

    added popup, slider, video and accordion

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import './main'
 import {priceHolder} from "@/components/price-holder/price-holder"
 import {accordion} from "@/components/accordion/accordion"
 import 'slick-carousel'
+import 'magnific-popup'
 
 priceHolder();
 accordion();
+
+$('.js-open-popup-video').magnificPopup({
+    type: 'iframe'
+})
+
+$('.js-popup-subscribe__submit').click(function () {
+    const gratitude = $('#gratitude').removeClass('mfp-hide')
+
+    $('.popup__container').html('').append(gratitude)
+    setTimeout($.magnificPopup.close, 1000)
+})
+$('.js-open-popup-subscribe').magnificPopup({
+    removalDelay: 300,
+    mainClass: 'mfp-fade'
+})
 
 $('.feedback').slick({
     infinite: false,
